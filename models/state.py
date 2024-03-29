92d3d12a7c41a8e4236c11c6375279bc290500b4
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-29 23:55:01 +0100

    add FS and DB relationship

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
+from models.city import City
+import models
 
 
 class State(BaseModel):
-    """ State class """
-    name = ""
+    """State class"""
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    @property
+    def cities(self):
+        """returns the list of City instances"""
+        instance_list = []
+        for ins in models.storage.all(City).values():
+            if ins.state_id == self.id:
+                instance_list.append(ins)
