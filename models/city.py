a42a84678523bbe070399d70e4d8d6dc050bb99e
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-29 23:38:19 +0100

    pycodestyle fix

--- models/city.py
+++ models/city.py
@@ -2,9 +2,12 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel
 from sqlalchemy import Column, ForeignKey, String
+
+
 class City(BaseModel):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
+    """The city class, contains state ID and name"""
+
+    __tablename__ = "cities"
 
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'),nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
