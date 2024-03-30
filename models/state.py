630f64c77b23e27cb64a206b9885b421ca2327e8
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-30 03:38:44 +0100

    fix error

--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
 from models.base_model import Base
+from os import getenv
 from models.base_model import BaseModel
 from models.city import City
 import models
@@ -13,12 +14,15 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="delete")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="delete")
+    else:
 
-    @property
-    def cities(self):
-        """returns the list of City instances"""
-        instance_list = []
-        for ins in models.storage.all(City).values():
-            if ins.state_id == self.id:
-                instance_list.append(ins)
+        @property
+        def cities(self):
+            """returns the list of City instances"""
+            instance_list = []
+            for ins in models.storage.all(City).values():
+                if ins.state_id == self.id:
+                    instance_list.append(ins)
+            return instance_list
