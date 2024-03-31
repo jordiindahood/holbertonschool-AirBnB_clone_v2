ac8fecfb845fb07ac02c61a0f6be95e7c91524d4
jordiindahood <hazem.ouni123@gmail.com>
2024-03-31 11:12:41 +0100

    fixing pycodestyle for test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -39,8 +39,10 @@ class TestPlace(unittest.TestCase):
         cls.state = State(name="California")
         cls.city = City(name="San Francisco", state_id=cls.state.id)
         cls.user = User(email="poppy@holberton.com", password="betty98")
-        cls.place = Place(city_id=cls.city.id, user_id=cls.user.id, name="Betty")
-        cls.review = Review(text="stellar", place_id=cls.place.id, user_id=cls.user.id)
+        cls.place = Place(city_id=cls.city.id, user_id=cls.user.id, name="B\
+            etty")
+        cls.review = Review(text="stel\
+            lar", place_id=cls.place.id, user_id=cls.user.id)
         cls.amenity = Amenity(name="water", place=cls.place.id)
         cls.filestorage = FileStorage()
 
@@ -103,7 +105,8 @@ class TestPlace(unittest.TestCase):
         self.assertTrue(hasattr(us, "latitude"))
         self.assertTrue(hasattr(us, "longitude"))
 
-    @unittest.skipIf(type(models.storage) == FileStorage, "Testing FileStorage")
+    @unittest.skipIf(type(models.storage) == FileStorage, "Testing \
+        FileStorage")
     def test_nullable_attributes(self):
         """Test that email attribute is non-nullable."""
         with self.assertRaises(OperationalError):
@@ -171,8 +174,10 @@ class TestPlace(unittest.TestCase):
         s = self.place.__str__()
         self.assertIn("[Place] ({})".format(self.place.id), s)
         self.assertIn("'id': '{}'".format(self.place.id), s)
-        self.assertIn("'created_at': {}".format(repr(self.place.created_at)), s)
-        self.assertIn("'updated_at': {}".format(repr(self.place.updated_at)), s)
+        self.assertIn("'created_at': \
+            {}".format(repr(self.place.created_at)), s)
+        self.assertIn("'updated_at': \
+            {}".format(repr(self.place.updated_at)), s)
         self.assertIn("'city_id': '{}'".format(self.place.city_id), s)
         self.assertIn("'user_id': '{}'".format(self.place.user_id), s)
         self.assertIn("'name': '{}'".format(self.place.name), s)
@@ -186,7 +191,8 @@ class TestPlace(unittest.TestCase):
         with open("file.json", "r") as f:
             self.assertIn("Place." + self.place.id, f.read())
 
-    @unittest.skipIf(type(models.storage) == FileStorage, "Testing FileStorage")
+    @unittest.skipIf(type(models.storage) == FileStorage, "Testing \
+        FileStorage")
     def test_save_dbstorage(self):
         """Test save method with DBStorage."""
         old = self.place.updated_at
@@ -217,8 +223,10 @@ class TestPlace(unittest.TestCase):
         self.assertEqual(dict, type(place_dict))
         self.assertEqual(self.place.id, place_dict["id"])
         self.assertEqual("Place", place_dict["__class__"])
-        self.assertEqual(self.place.created_at.isoformat(), place_dict["created_at"])
-        self.assertEqual(self.place.updated_at.isoformat(), place_dict["updated_at"])
+        self.assertEqual(self.place.created_at.isoformat(), place_dict["\
+            created_at"])
+        self.assertEqual(self.place.updated_at.isoformat(), place_dict["\
+            updated_at"])
         self.assertEqual(self.place.city_id, place_dict["city_id"])
         self.assertEqual(self.place.user_id, place_dict["user_id"])
         self.assertEqual(self.place.name, place_dict["name"])
