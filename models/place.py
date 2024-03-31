94818ad70f65261eab2ed3d83a5676071d677a2e
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-31 05:54:59 +0100

    fix pycodestyle

--- models/place.py
+++ models/place.py
@@ -13,10 +13,10 @@ from models.review import Review
 place_amemity = Table("place_amenity", Base.metadata,
                           Column("place_id", String(60),
                                  ForeignKey("places.id"),
-                                 primary_key=True, nullable=False),
+                                 primary_key=True),
                           Column("amenity_id", String(60),
                                  ForeignKey("amenities.id"),
-                                 primary_key=True, nullable=False))
+                                 primary_key=True))
 class Place(BaseModel, Base):
     """A place to stay"""
 
