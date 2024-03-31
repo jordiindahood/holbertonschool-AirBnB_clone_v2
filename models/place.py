a70a13cb6169f24dac90d7bdbb2b1a648dec39a1
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-31 06:36:44 +0100

    fix pycodestyle

--- models/place.py
+++ models/place.py
@@ -10,13 +10,15 @@ import models
 from sqlalchemy import Table
 from models.review import Review
 
-place_amemity = Table("place_amenity", Base.metadata,
-                          Column("place_id", String(60),
-                                 ForeignKey("places.id"),
-                                 primary_key=True),
-                          Column("amenity_id", String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True))
+place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True,
+                                 nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True,
+                                 nullable=False))
 class Place(BaseModel, Base):
     """A place to stay"""
 
