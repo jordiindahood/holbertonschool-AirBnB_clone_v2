e5729f85027f7775ec92b2ddcabdfb509ce9e3ff
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-31 07:34:31 +0100

    fix pycodestyle

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, ForeignKey, String
-from models.base_model import Base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+STO_TYP = getenv('HBNB_TYPE_STORAGE')
 
 
 class Amenity(BaseModel, Base):
-    """
-    Amenity class representation
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    if STO_TYP == "db":
+        from models.place import place_amenity
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary=place_amenity)
+    else:
+        name = ''
