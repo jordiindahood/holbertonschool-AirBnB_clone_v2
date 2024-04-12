af22fdab5260ddeedd9a18ef16f3d307d2359117
jordiindahood <hazem.ouni123@gmail.com>
2024-04-12 11:24:16 +0100

    script 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""start a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """return a message"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """return a message"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    # Run the Flask application
+    app.run(host="0.0.0.0", port=5000)
