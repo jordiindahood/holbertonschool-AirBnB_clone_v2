9d6bbc8054e95cbbeaa63576ece88bb9ead4c36b
jordiindahood <hazem.ouni123@gmail.com>
2024-04-12 21:03:16 +0100

    script 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""start a Flask web application"""
+from flask import Flask
+from markupsafe import escape
+from flask import render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Returns the list of all States in the database."""
+    states = sorted(storage.all("State").values(), key=lambda s: s.name)
+    return render_template("7-states_list.html", states=states)
+
+@app.teardown_appcontext
+def close(self):
+    """closes the database session."""
+    storage.close()
+
+if __name__ == "__main__":
+    # Run the Flask application
+    app.run(host="0.0.0.0", port=5000)
