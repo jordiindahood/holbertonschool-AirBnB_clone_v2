f28bdc3d716f67cdccc2978206ef9cf168e7b532
jordiindahood <hazem.ouni123@gmail.com>
2024-04-12 21:04:49 +0100

    script 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,16 +8,19 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Returns the list of all States in the database."""
     states = sorted(storage.all("State").values(), key=lambda s: s.name)
     return render_template("7-states_list.html", states=states)
 
+
 @app.teardown_appcontext
 def close(self):
     """closes the database session."""
     storage.close()
+
 
 if __name__ == "__main__":
     # Run the Flask application
