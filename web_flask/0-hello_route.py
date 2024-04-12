9ee8346ca8ba2f98f9ebd3153c43fe7bc40a19eb
jordiindahood <hazem.ouni123@gmail.com>
2024-04-12 11:15:55 +0100

    pycodestyle ..

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """return a message"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     # Run the Flask application
